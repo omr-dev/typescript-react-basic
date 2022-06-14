3ae36e755c82d4b775d23d6bba0279e34b269a5c
omer <jagenau49@gmail.com>
2022-06-14 22:34:20 +0200

    raw book data loaded

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 
+import { useEffect, useState } from 'react'
+import axios from 'axios';
 import './App.scss'
 
 function App() {
-  
+  const [books, setBooks] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const books = (await axios.get('https://gutendex.com/books/?search=berlin')).data.results;
+      setBooks(books);
+      console.log(`There are ${books.length} books`, books);
+    })()
+  }, []);
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Hi!</h1>
+      {books.length > 0 ? <h2>{`There are ${books.length} books`}</h2> : <h2>Loading...</h2>}
     </div>
   )
 }
